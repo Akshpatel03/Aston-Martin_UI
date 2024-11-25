e3594bd7ba72f081f2ac54ccb09100e772c15e61
Prajesh Kapadiya <prajesh.kapadiya@tatvasoft.com>
2024-11-25 12:53:49 +0530

    changes

--- shared/routes.ts
+++ shared/routes.ts
@@ -1,6 +1,7 @@
 export const ROUTES = {
   DesignerHome: "/designer",
   DesignerNewCar: "/designer/new-car",
+  DesignerNewCarModel: "/designer/new-car-model",
   DesignerPreOwned: "/pre-owned",
   DesignerBookService: "/book-service",
   DesignerValueMyAstonMartin: "/value",
