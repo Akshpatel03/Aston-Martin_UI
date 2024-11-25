d7ff0c8bf73f0c7e0f6ed1da38d9df2380a86117
Nikhil Vaya <nikhilvaya5@outlook.com>
2024-11-25 16:33:58 +0530

    salesforce integration

--- components/layout/sidebar/index.tsx
+++ components/layout/sidebar/index.tsx
@@ -7,48 +7,47 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 
 const Sidebar = () => {
-
-    return (
-        <aside className="sidebar">
-            <div className="head">
-                <Navbar.Brand href="#">
-                    <Image src={images.DealerLogo} alt="Dealer" />
-                </Navbar.Brand>
-            </div>
-            <ul className="main-navigation">
-                <li className="nav-link">
-                    <NextLink className={`nav-item`} href={ROUTES.DesignerHome} title="Cars">
-                        New Cars
-                    </NextLink>
-                </li>
-                <li className="nav-link">
-                    <NextLink className="nav-item" href={ROUTES.DesignerHome} title="Cars">
-                        Pre-owned Cars
-                    </NextLink>
-                </li>
-                <li className="nav-link">
-                    <NextLink className="nav-item" href={ROUTES.DesignerHome} title="Repairs">
-                        Servicing & Repairs
-                    </NextLink>
-                </li>
-                <li className="nav-link">
-                    <NextLink className="nav-item" href={ROUTES.DesignerHome} title="Car">
-                        Value My Car
-                    </NextLink>
-                </li>
-                <li className="nav-link">
-                    <NextLink className="nav-item" href={ROUTES.DesignerHome} title="News">
-                        News
-                    </NextLink>
-                </li>
-                <li className="nav-link">
-                    <NextLink className="nav-item" href={ROUTES.DesignerHome} title="Contact">
-                        Contact
-                    </NextLink>
-                </li>
-            </ul>
-        </aside>
-    );
+  return (
+    <aside className="sidebar">
+      <div className="head">
+        <Navbar.Brand href="#">
+          <Image src={images.DealerLogo} alt="Dealer" />
+        </Navbar.Brand>
+      </div>
+      <ul className="main-navigation">
+        <li className="nav-link">
+          <NextLink className={`nav-item`} href={ROUTES.Home} title="Cars">
+            New Cars
+          </NextLink>
+        </li>
+        <li className="nav-link">
+          <NextLink className="nav-item" href={ROUTES.Home} title="Cars">
+            Pre-owned Cars
+          </NextLink>
+        </li>
+        <li className="nav-link">
+          <NextLink className="nav-item" href={ROUTES.Home} title="Repairs">
+            Servicing & Repairs
+          </NextLink>
+        </li>
+        <li className="nav-link">
+          <NextLink className="nav-item" href={ROUTES.Home} title="Car">
+            Value My Car
+          </NextLink>
+        </li>
+        <li className="nav-link">
+          <NextLink className="nav-item" href={ROUTES.Home} title="News">
+            News
+          </NextLink>
+        </li>
+        <li className="nav-link">
+          <NextLink className="nav-item" href={ROUTES.Home} title="Contact">
+            Contact
+          </NextLink>
+        </li>
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
